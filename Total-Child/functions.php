79bd3350efbe0cc3fa2029a1f20405b113ca5781
AJ Clarke <aj@Alexanders-iMac-2.local>
2014-07-28 11:49:43 -0700

    Initial Upload

---
+++ Total-Child/functions.php
@@ -0,0 +1,6 @@
+<?php
+// Start adding some code here, don't be shy!
+// If you want to override core theme functions you can.
+// Open the parent "Total" theme and inside the "functions" folder you'll find all the main functions.
+// Pretty much any function from the parent theme can be copied and pasted here and then tweaked to your liking.
+// You can obviously delete all this, just make sure you leave the <?php at the top!
