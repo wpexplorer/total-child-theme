52b5d8f2074d0fdff3508cd456dbefcb7847fc7a
WPExplorer <wpexplorer@gmail.com>
2016-06-06 15:08:16 -0700

    Added parent theme version number to stylesheet param

--- functions.php
+++ functions.php
@@ -16,8 +16,17 @@
 
 /**
  * Load the parent style.css file
+ *
+ * @link http://codex.wordpress.org/Child_Themes
  */
 function total_child_enqueue_parent_theme_style() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+
+	// Dynamically get version number of the parent stylesheet (lets browsers re-cache your stylesheet when you update your theme)
+	$theme   = wp_get_theme( 'Total' );
+	$version = $theme->get( 'Version' );
+
+	// Load the stylesheet
+    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css', array(), $version );
+
 }
 add_action( 'wp_enqueue_scripts', 'total_child_enqueue_parent_theme_style' );
