fb53959469301575488056a9ab1f4e73a872f850
WPExplorer <wpexplorer@gmail.com>
2021-11-10 14:12:00 -0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -19,7 +19,7 @@ function total_child_enqueue_parent_theme_style() {
 		'parent-style',
 		get_template_directory_uri() . '/style.css',
 		array(),
-		wp_get_theme()->get( 'Version' )
+		wp_get_theme( 'Total' )->get( 'Version' )
 	);
 }
 add_action( 'wp_enqueue_scripts', 'total_child_enqueue_parent_theme_style' );
